7d7d179519b7e3459080909a597e318c600387a5
uttpa <uttpa@student.kit.edu>
2025-07-12 20:08:01 +0200

    More Java-Doc

--- src/launcher/Starter.java
+++ src/launcher/Starter.java
@@ -2,12 +2,25 @@ package launcher;
 
 import view.UserInterface;
 
+/**
+ * This class starts the application.
+ * @author ManaOverflow
+ */
 public final class Starter {
     private Starter() {
         // No use
     }
+
+    /**
+     * This method starts the program.
+     * @param args  the command line arguments.
+     */
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface(System.in, System.out);
-        userInterface.run();
+        if (args.length != 0) {
+            System.out.print("Error, invalid command line arguments.");
+        } else {
+            UserInterface userInterface = new UserInterface(System.in, System.out);
+            userInterface.run();
+        }
     }
 }
---
+++ src/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package provides a Text Generator based on markov graphs.
+ * @author ManaOverflow
+ */
